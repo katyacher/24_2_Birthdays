7c95c8e575622cab34f20e2ea6ec2969734eae86
kate <katyacher@inbox.ru>
2022-11-25 17:45:06 +0300

    Chack date in while-circle was added, func print_vec() and print_next_birthday() addad. The difference calculated using structure std::tm, sorted by map container.

--- main.cpp
+++ main.cpp
@@ -2,39 +2,83 @@
 #include <ctime>
 #include <iomanip>
 #include <map>
+#include <vector>
 
-int main() {
-    std::cout << "24.1. My Friends BirthDay" << std::endl;
-    std::time_t current_t = std::time(nullptr);
-    std::tm* current_tm = std::localtime(&current_t);
-    std::multimap <std::string, std::tm> birthdays;
+struct my_friend{
     std::string name;
     std::tm birthday{};
+    double diff = 0;
+};
+
+void print_vec(std::vector<my_friend> vec);
+void print_next_birthday( std::map<double, std::vector<my_friend>> bro_map);
+
+int main() {
+    std::cout << "24.1. My Friends Birthdays" << std::endl;
+
+    std::time_t current_t = std::time(nullptr);
+    std::tm* current_tm = std::localtime(&current_t);
+
+    std::vector <my_friend> my_friends;
 
     while(true){
+        my_friend bro;
         std::cout << "Name: ";
-        std::cin >> name;
-        if(name == "end") {
-            break;
-        } else{
+        std::cin >> bro.name;
+
+        if(bro.name == "end") break;
+
+        while(true){
             std::cout << "Birthday (YYYY/MM/DD): ";
-            std::cin >> std::get_time(&birthday, "%Y/%m/%d");
-            birthdays.emplace(name, birthday);
+            std::cin >> std::get_time(&bro.birthday, "%Y/%m/%d");
+            if(std::cin.fail()){
+                std::cin.clear();
+                std::string str;
+                std::getline(std::cin, str);
+                std::cout << "Incorrect format of date. Try again." << std::endl;
+            } else {
+                if(mktime(&bro.birthday) <= mktime(current_tm)){
+                    my_friends.push_back(bro);
+                    break;
+                } else {
+                    std::cout << "Birthday can't be in the future. Try again" << std::endl;
+                }
+            }
         }
     }
 
-    std::multimap<double,std::string> next;
+    std::tm today_tm = {};
+    today_tm.tm_mon = current_tm->tm_mon;
+    today_tm.tm_mday = current_tm->tm_mday;
 
-    for(auto my_friend: birthdays) {
-        my_friend.second.tm_year = current_tm->tm_year;
-        double diff = difftime(current_t, mktime(&my_friend.second));
-        next.emplace(std::abs(diff), my_friend.first);
+    std::map<double, std::vector<my_friend>> next_birthday_in_this_year;
 
-        if(my_friend.second.tm_mon == current_tm->tm_mon
-            && my_friend.second.tm_mday == current_tm->tm_mday){
-            std::cout << "Today is "<< my_friend.first << "'s birthday. " ;
+    for(auto item: my_friends) {
+        item.birthday.tm_year = today_tm.tm_year;
+        item.diff = difftime( mktime(&item.birthday), mktime(&today_tm));
+
+        if(item.diff >= 0) {
+            next_birthday_in_this_year[item.diff].push_back(item);
         }
     }
-    std::cout << next.begin()->second << std::endl;
+
+    print_next_birthday(next_birthday_in_this_year);
+
     return 0;
+}
+
+void print_vec(std::vector<my_friend> vec){
+    for(auto item:vec){
+        std::cout << item.name << " " << std::put_time(&item.birthday, "%m/%d" ) << std::endl;
+    }
+};
+
+void print_next_birthday( std::map<double, std::vector<my_friend>> bro_map){
+    if(bro_map.begin()->first == 0){
+        std::cout << "Today is birthday of: " << std::endl;
+        print_vec(bro_map.begin()->second);
+    } else {
+        std::cout << "Friend's next birthday: " << std::endl;
+        print_vec(bro_map.begin()->second);
+    }
 }
